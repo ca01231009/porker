df2636a1814512b329a0ab8047c77a836a487708
ca01231009 <ca01231009@st.kawahara.ac.jp>
2023-10-10 14:42:01 +0900

    途中経過

--- cpu_change.js
+++ cpu_change.js
@@ -14,7 +14,7 @@ cpuSplice = (cpu_hand, cpu_rank) => {
             cpu_hand = normalCpuChange(cpu_hand, cpu_rank);
             return cpu_hand;
             break;
-
+            
             case 1:
             cpu_hand = jokerCpuChange(cpu_hand, cpu_rank);
             returnAce(cpu_hand);
@@ -22,11 +22,12 @@ cpuSplice = (cpu_hand, cpu_rank) => {
             return cpu_hand;
             break;
         };
+    // returnAce(cpu_hand);
     console.log(cpu_hand);
 }
 
 cpuAdd =(cpu_hand)=>{
-
+    
     let addarray = cards.slice(0, 5 - cpu_hand.length);
     cards.splice(0, 5 - cpu_hand.length);
     Array.prototype.push.apply(cpu_hand, addarray);
@@ -34,12 +35,30 @@ cpuAdd =(cpu_hand)=>{
     return cpu_hand;
 }
 
-
 normalCpuChange = (cpu_hand, cpu_rank) => {
     sort(cpu_hand);
     cpu_rank = rank_judge(cpu_hand);
     switch (cpu_rank) {
         case 10:
+            // cpu_hand = sameMarkMock(cpu_hand);
+            // console.log(cpu_hand.length);
+            // if (cpu_hand.length == 5) {
+            //     cpu_hand = stairMock(cpu_hand);
+            //     return cpu_hand;
+            // }
+            // console.log(cpu_hand.length);
+            // if (cpu_hand.length == 5) {
+            //     cpu_hand = sameThreeMark(cpu_hand);
+            //     return cpu_hand;
+            // };
+            // console.log(cpu_hand.length);
+            // if (cpu_hand.length == 5) {
+            //     cpu_hand = [];
+            //     return cpu_hand;
+            // };
+            // return cpu_hand;
+            // break;
+
             cpu_hand = sameMarkMock(cpu_hand);            
             if (cpu_hand.length == 4) {
                 return cpu_hand;
@@ -87,8 +106,33 @@ normalCpuChange = (cpu_hand, cpu_rank) => {
     return cpu_hand;
 }
 
+jokerCpuChange = (cpu_hand, cpu_rank) => {
+    sort(cpu_hand);
+    switch (cpu_rank) {
+        case 9:
+            cpu_hand = sameMarkMock(cpu_hand);
+            if (cpu_hand.length == 3) {
+                return cpu_hand
+                break;
+            };
+            cpu_hand = onePairstairMock(cpu_hand);
+            if (cpu_hand.length == 3) {
+                return cpu_hand;
+                break;
+            };
+            cpu_hand = [];
+            return cpu_hand;
+            break;
 
-//同じマークが手札-1枚か？
+        case 7:
+            cpu_hand = jokerThreeCardSampling(cpu_hand);
+            return cpu_hand;
+            break;
+    };
+}
+
+
+//同じマーク4つ？
 sameMarkMock = (cpu_hand) => {
     let cpu_hand2 = cpu_hand.slice();
     for (var i = 0; i < cpu_hand.length; i++) {
@@ -104,8 +148,7 @@ sameMarkMock = (cpu_hand) => {
     return cpu_hand2;
 }
 
-
-//あと1枚で階段？
+//豚さんの時階段なりそう？
 stairMock = (cpu_hand, joker_flg) => {
     sort(cpu_hand);
     let cpu_hand2 = cpu_hand.slice();
@@ -125,7 +168,7 @@ stairMock = (cpu_hand, joker_flg) => {
     return cpu_hand2;
 }
 
-//同じマークが手札-2枚か？
+//豚の時マーク3つ？
 sameThreeMark = (cpu_hand) => {
     let cpu_hand2 = cpu_hand.slice();
     for (var i = 0; i < 4; i++) {
@@ -152,27 +195,7 @@ sameThreeMark = (cpu_hand) => {
     };
 }
 
-
 //ワンペア時、ペアの2枚を取る
 onePairSampling = (cpu_hand) => {
     sort(cpu_hand);
-    for (var i = 0; cpu_hand[i].num != cpu_hand[i + 1].num; i++) {};
-    cpu_hand = cpu_hand.splice(i, 2);
-    return cpu_hand;
-}
-
-
-//ツーペア時、残りの一枚を抜き取る
-twoPairSampling = (cpu_hand) => {
-    sort(cpu_hand);
-    if (cpu_hand[1].num == cpu_hand[2].num && cpu_hand[3].num == cpu_hand[4].num) {
-        cpu_hand.splice(0, 1);
-        return cpu_hand;
-    } else if (cpu_hand[0].num == cpu_hand[1].num && cpu_hand[3].num == cpu_hand[4].num) {
-        cpu_hand.splice(2, 1);
-        return cpu_hand;
-    } else if (cpu_hand[0].num == cpu_hand[1].num && cpu_hand[2].num == cpu_hand[3].num) {
-        cpu_hand.splice(4, 1);
-    };
-    return cpu_hand;
 }
